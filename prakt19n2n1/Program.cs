b089c1fd67e28d951404ba8ada886987a88e3ec8
Atamanyuk2008 <atamaniuk.aleksandr@gmail.com>
2025-02-02 22:32:27 +0200

    Add files via upload

---
+++ prakt19n2n1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakt19n2n1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a1 = 1;
+            int d = 4;
+
+
+            for (int a = 1; a <= 5; a++)
+            {
+                int an = a1 + (a - 1) * d;
+                Console.Write(an + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
