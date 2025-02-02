1dd172b35bc31d113ce3430af3319661a7b04438
Atamanyuk2008 <atamaniuk.aleksandr@gmail.com>
2025-02-02 22:34:01 +0200

    Add files via upload

---
+++ prakt16n3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prakt16n3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int q = 0; q < 16; q++)
+            {
+                Console.Write("#");
+            }
+            for (int e = 0; e < 16 - 2; e++)
+            {
+                Console.Write("#");
+                for (int j = 0; j < 16 - 2; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine("#");
+            }
+            for (int d = 0; d < 16; ds++)
+            {
+                Console.Write("#");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
